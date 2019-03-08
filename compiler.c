d011f72c86ef7e2b45c49021c44f998a0b67565d
Dogeko <dogeko852@gmail.com>
2019-03-07 17:23:52 -0800

    gachiBASS

--- compiler.c
+++ compiler.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	printf("Hello world!\n");
 	char str1[20];
+	char str2[20] = "exit";
 	while(1) {
+		printf("command> ");
 		scanf("%s", str1);
 		printf("Inserted command: %s\n", str1);
+		int exiting = strcmp(str1, str2);
+		if(exiting == 0) {
+			break;
+		}
 		//hello my love
+		//stfu omar
 	}
-	return 0;
+	return(0);
 }
