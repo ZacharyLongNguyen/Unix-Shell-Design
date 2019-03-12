6a123a2c317ed8e39118ccd85329a457d9600bf7
RacistGiraffe <znguyenmdc30@gmail.com>
2019-03-12 16:57:03 -0700

    Test commit. Added some code to start existing programs

--- compiler.c
+++ compiler.c
@@ -5,15 +5,33 @@
 #include <err.h>
 
 int main() {
-	//Test line commit
 	printf("compiler> ");
 	char *str1 = NULL;
+	char *path1 = (char*)malloc(sizeof(char)*15);
+	char *path2 = (char*)malloc(sizeof(char)*20);
+	char *parser = NULL;
+	strcpy(path1, "/bin/");
+	strcpy(path2, "/usr/bin/");
 	size_t linesize = 0; // number of elements with elements being of a certain byte size
-	ssize_t linelen; // byte size of each element, or negative error value
+	ssize_t linelen, pathlen; // byte size of each element, or negative error value
 	while((linelen = getline(&str1, &linesize, stdin)) != -1) { // user input and input length
 		//strsep(&str1, " ");
 		if (strncmp("exit", str1, 4) == 0) { // check if input = "exit", return 0 if true
 			exit(0);
+		}
+		else if (strncmp("cd", str1, 2) == 0) {
+			//if cd is input
+			//Need to implement this
+			printf("cd");
+		}
+		pathlen = linelen - 1;
+		strncat(path1, str1,pathlen);
+		strncat(path2, str1,pathlen);
+		if(access(path1, X_OK) == 0) {
+			printf("Command exists in path1");
+		}
+		else if(access(path2, X_OK) == 0) {
+			printf("Command exists in path2");
 		}
 		printf("Input command: ");
 		fwrite(str1, linelen, 1, stdout); // rewrites the input line
@@ -26,9 +44,6 @@ int main() {
 			else if (rc == 0) {
 				*/
 		printf("compiler> ");
-		//hello my love
-		//stfu omar
-		//suck dick darren, Love you too.
 	}
 	free(str1); // deallocates memory from str1
 	if(ferror(stdin)) { // checks if there is an error, returns 0 if no error
