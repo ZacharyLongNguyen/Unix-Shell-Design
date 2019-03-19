dabbbd7dddf494dcba6c670d539dc58274c6809a
Dogeko <dogeko852@gmail.com>
2019-03-19 15:54:18 -0700

    dab if you think

--- compiler.c
+++ compiler.c
@@ -26,17 +26,23 @@ int main() {
 	char *dir = "";
 	char *path1 = (char*)malloc(sizeof(char)*15);
 	char *path2 = (char*)malloc(sizeof(char)*20);
-	char *parser = NULL;
 	strcpy(path1, "/bin/");
 	strcpy(path2, "/usr/bin/");
 	size_t linesize = 0; // number of elements with elements being of a certain byte size
 	ssize_t linelen, pathlen; // byte size of each element, or negative error value
 	while((linelen = getline(&str1, &linesize, stdin)) != -1) { // user input and input length
 		//strsep(&str1, " ");
-		if (strncmp("exit", str1, 4) == 0) { // check if input = "exit", return 0 if true
-			exit(0);
+		str1 = strtok(str1, " \n"); // string for command
+		dir = strtok(NULL, " \n"); // string for directory
+		printf("%s \n", str1); // just for testing
+		printf("%s \n", dir); // just for testing
+		if (dir == NULL) {
+			dir = "(null)";
+			if (strncmp("exit", str1, 4) == 0) { // check if input = "exit", return 0 if true
+				exit(0);
+			}
 		}
-		else if (strncmp("cd", str1, 2) == 0) {
+		if (strncmp("cd", str1, 2) == 0) {
 			//if cd is input
 			//Need to implement this
 			dir = strtok(str1, " \n");
@@ -77,19 +83,12 @@ int main() {
 			}
 			else if (rc == 0) {
 				*/
-		printf("SJShell> %s", dir);
+		printf("SJShell> ");
 	}
 	free(str1); // deallocates memory from str1
+	free(dir); // deallocates memory from dir
 	if(ferror(stdin)) { // checks if there is an error, returns 0 if no error
 		err(1, "getline"); // displays an error message with where the error is
 	}
 }
-
-//Parser function
-/*
-char ** parser (char *str1) {
-	char *parsed;
-	parsed = strtok (str1, " ");
-}
-*/
 
