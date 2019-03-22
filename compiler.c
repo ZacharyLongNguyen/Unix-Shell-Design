3287711a6970fb27b10b0bee9196ed5ece74a98c
RacistGiraffe <znguyenmdc30@gmail.com>
2019-03-22 16:06:13 -0700

    Finished cd function

--- compiler.c
+++ compiler.c
@@ -6,6 +6,7 @@
 
 extern FILE *stderr;
 char ** parser(char *);
+char *currentDir;
 int main() {
 	/*
 		TODO:
@@ -17,14 +18,19 @@ int main() {
 		-From Larkin: the 'cd' command should call the chdir() function, where the input is what comes after
 		 the cd command
 	*/
+	char buff[1000];
+	currentDir = getcwd(buff, sizeof(buff));
 	printf("MemeShell> ");
 	char *str1 = NULL;
 	char **args;
-	char *dir = "";
+	char **dir = malloc(8*sizeof(char*));
 	char *path1 = (char*)malloc(sizeof(char)*15);
 	char *path2 = (char*)malloc(sizeof(char)*20);
 	strcpy(path1, "/bin/");
 	strcpy(path2, "/usr/bin/");
+	char *defaultPath = malloc(sizeof(char*));
+	strcpy(defaultPath, "/bin/");
+	dir[0] = defaultPath;
 	size_t linesize = 0; // number of elements with elements being of a certain byte size
 	ssize_t linelen, pathlen; // byte size of each element, or negative error value
 	while((linelen = getline(&str1, &linesize, stdin)) != -1) { // user input and input length
@@ -42,10 +48,21 @@ int main() {
 			exit(0);
 		}
 		else if (strncmp("cd", str1, 2) == 0) { //checks if cd is input
-			dir = strtok(str1, " \n");
-			dir = strtok (NULL, " ");
-			dir[strlen(dir) - 1] = '\0'; //Remove the end line character at the end of dir
+			args = parser(str1);
+			if(args[2] != NULL) 
+				printf("Too many arguments passed for cd \n");
+			else if(args[1] == NULL) {
+				int cdcheck = chdir(currentDir);
+				if(cdcheck < 0) 
+					printf("Bad address for cd\n");
+			
 			}
+			else
+				chdir(args[1]);
+			//dir = strtok(str1, " \n");
+			//dir = strtok (NULL, " ");
+			//dir[strlen(dir) - 1] = '\0'; //Remove the end line character at the end of dir
+		}
 		else if (strncmp("path", str1, 4) == 0) {
 			//if input is path
 			//Need to implement this
@@ -54,6 +71,7 @@ int main() {
 		else { //The input is already an existing function, or doesn't exist
 			//pathlen = linelen - 1;
 			//char **args = parser(str1); //Places arguments after command into args char
+			args = parser(str1);
 			strncat(path1, str1,pathlen); //
 			strncat(path2, str1,pathlen);
 			if(access(path1, X_OK) == 0) {
@@ -81,15 +99,14 @@ int main() {
 		//printf("\n");
 		printf("MemeShell%s> ", dir);
 	}
-	free(str1); // deallocates memory from str1
-	free(dir); // deallocates memory from dir
-	char error_message[30] = "An error has occurred\n";
-	write(STDERR_FILENO, error_message, strlen(error_message));
+	//free(str1); // deallocates memory from str1
+	//free(dir); // deallocates memory from dir
+	//char error_message[30] = "An error has occurred\n";
+	//write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-/*
 char ** parser (char *input) {
-	input = strtok(input, " \n");
+	input = strtok(input, "\n");
 	char *parsed;
 	char **args = malloc(8*sizeof(char *));
 	int argSize = 0;
@@ -101,5 +118,5 @@ char ** parser (char *input) {
 	}
 	args[argSize] = NULL;
 	return args;
-}*/
+}
 
